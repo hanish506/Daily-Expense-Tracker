31979082a9942a66cb553f81e64b233305f5b32b
hanish <hanishr.it24@bitsathy.ac.in>
2026-02-05 10:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+let total = 0;
+
+function addExpense() {
+  const name = document.getElementById("name").value;
+  const amount = document.getElementById("amount").value;
+  const category = document.getElementById("category").value;
+
+  if (name === "" || amount === "" || category === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  const list = document.getElementById("expenseList");
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+    ${name} (${category}) - ₹${amount}
+    <button onclick="removeExpense(this, ${amount})">Delete</button>
+  `;
+
+  list.appendChild(li);
+
+  total += Number(amount);
+  document.getElementById("total").textContent = total;
+
+  document.getElementById("name").value = "";
+  document.getElementById("amount").value = "";
+  document.getElementById("category").value = "";
+}
+
+function removeExpense(btn, amount) {
+  btn.parentElement.remove();
+  total -= Number(amount);
+  document.getElementById("total").textContent = total;
+}
 
